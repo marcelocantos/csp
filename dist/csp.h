789b60a73fde827195e1f4f744762c662ef5966e
Marcelo Cantos <marcelo.cantos@gmail.com>
2026-07-11 15:07:30 +1000

    Coin-flip third-pairing deadlock + stack-analysis ASan fix (#93)

--- dist/csp.h
+++ dist/csp.h
@@ -7,9 +7,9 @@
 
 /* csp/csp.h */
 
-#define CSP_VERSION "0.21.0"
+#define CSP_VERSION "0.22.0"
 #define CSP_VERSION_MAJOR 0
-#define CSP_VERSION_MINOR 21
+#define CSP_VERSION_MINOR 22
 #define CSP_VERSION_PATCH 0
 
 
