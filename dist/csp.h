8fbfab5e13873ef1b2ead366b05e42d21ebbc5c8
Marcelo Cantos <marcelo.cantos@gmail.com>
2026-07-26 08:16:21 +1000

    Bump version to 0.25.0
    CSP_VERSION macros and STABILITY.md table; dist regenerated.

--- dist/csp.h
+++ dist/csp.h
@@ -7,9 +7,9 @@
 
 /* csp/csp.h */
 
-#define CSP_VERSION "0.24.0"
+#define CSP_VERSION "0.25.0"
 #define CSP_VERSION_MAJOR 0
-#define CSP_VERSION_MINOR 24
+#define CSP_VERSION_MINOR 25
 #define CSP_VERSION_PATCH 0
 
 
