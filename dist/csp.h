875a1ba298b9198446128f1f92614d2aa73eb631
Marcelo Cantos <marcelo.cantos@gmail.com>
2026-07-31 22:20:25 +1000

    Bump version to 0.29.0
    Stack-analysis productization wave: concrete spawn analysis, data-path
    pre-scan, fan-in unification, ASan shell margin.

--- dist/csp.h
+++ dist/csp.h
@@ -7,9 +7,9 @@
 
 /* csp/csp.h */
 
-#define CSP_VERSION "0.28.0"
+#define CSP_VERSION "0.29.0"
 #define CSP_VERSION_MAJOR 0
-#define CSP_VERSION_MINOR 28
+#define CSP_VERSION_MINOR 29
 #define CSP_VERSION_PATCH 0
 
 
--- include/csp/csp.h
+++ include/csp/csp.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define CSP_VERSION "0.28.0"
+#define CSP_VERSION "0.29.0"
 #define CSP_VERSION_MAJOR 0
-#define CSP_VERSION_MINOR 28
+#define CSP_VERSION_MINOR 29
 #define CSP_VERSION_PATCH 0
 
 #include <csp/internal/log.h>
